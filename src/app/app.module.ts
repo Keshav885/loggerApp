7ca4c78ee357a0f2aca53fa076b74f8a9d920457
Keshav885 <kesava.paruchuri@aurotechcorp.com>
2018-07-20 10:11:51 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import { MatButtonModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { MatFormFieldModule } from '@angular/material';
+import {FormsModule} from '@angular/forms';
+
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
+    MatButtonModule,
+    MatCardModule,
+    FormsModule,
+    MatInputModule,
+    // MatFormFieldModule,
+    BrowserAnimationsModule,
     BrowserModule
   ],
   providers: [],
